f2dd3f8d7210c6097e409646fba611c0a46502ff
Cleiton França <cleiton2281@gmail.com>
2017-07-17 21:22:34 -0300

    para o servidor

--- www/js/services/IngressoService.js
+++ www/js/services/IngressoService.js
@@ -2,8 +2,8 @@ angular.module('app.serviceIngresso', [])
 
 
     .service('IngressoService', ['$http', function ($http) {
-       // const SERVIDOR = "http://torcedordigital.com";
-        const SERVIDOR = "http://10.0.0.106:8080";
+        const SERVIDOR = "http://torcedordigital.com";
+        //const SERVIDOR = "http://10.0.0.106:8080";
 
         var service = {
             calendario: function () {
